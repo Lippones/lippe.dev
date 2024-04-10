ef90eb0f81a032f14c3463087c38b5cae3a4fc42
Filipe Vieira <filipe68ft@hotmail.com>
2024-04-10 00:06:37 -0300

    ✨ feat: add  tags in project

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,8 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        hostname: 'https://github.com',
+        protocol: 'https',
+        hostname: 'github.com',
       },
     ],
   },
--- src/components/home/projects.tsx
+++ src/components/home/projects.tsx
@@ -5,6 +5,8 @@ import { useRef } from 'react'
 import { Button } from '../ui/button'
 import { FaGithub } from 'react-icons/fa'
 import Link from 'next/link'
+import Image from 'next/image'
+import { Badge } from '../ui/badge'
 
 export function Projects() {
   const targetRef = useRef<HTMLDivElement | null>(null)
@@ -23,7 +25,7 @@ export function Projects() {
       className={`relative flex max-w-screen-2xl w-full mx-auto px-4 md:px-8`}
     >
       <div className="sticky h-screen top-0 flex items-center overflow-hidden">
-        <motion.div style={{ x }} className="flex">
+        <motion.div style={{ x }} className="flex flex-col md:flex-row">
           <div className="w-[400px] flex-shrink-0 mr-36">
             <h2 className="text-3xl font-bold">Projects</h2>
             <p className="text-sm mt-2 text-pretty leading-relaxed">
@@ -36,11 +38,17 @@ export function Projects() {
           <div className="flex items-center">
             <ul className="flex flex-shrink-0 gap-6">
               {Array.from({ length: 10 }).map((_, index) => (
-                <li
-                  className="project bg-white rounded-2xl w-[600px] h-[600px] shadow-lg p-4"
-                  key={index}
-                >
-                  Project {index + 1}
+                <li key={index}>
+                  <ProjectCard
+                    project={{
+                      coverImageUrl: '/web-landing.png',
+                      description:
+                        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat architecto sunt tempore, totam fugit ea atque dignissimos obcaecati voluptate ad quisquam nemo, odio facere tenetur voluptatum dolor eligendi animi delectus!',
+                      title: 'Project Title',
+                      videoUrl: 'https://www.youtube.com/watch?v=9bZkp7q19f0',
+                    }}
+                    tags={['React', 'TypeScript', 'TailwindCSS']}
+                  />
                 </li>
               ))}
             </ul>
@@ -60,5 +68,39 @@ export function Projects() {
         </motion.div>
       </div>
     </section>
+  )
+}
+
+interface ProjectCardProps {
+  project: {
+    title: string
+    description: string
+    coverImageUrl: string
+    videoUrl: string
+  }
+  tags: string[]
+}
+
+export function ProjectCard({ project, tags }: ProjectCardProps) {
+  return (
+    <div className="project rounded-2xl text-zinc-950 w-[600px] h-[600px] shadow-lg relative overflow-hidden">
+      <Image
+        src={project.coverImageUrl}
+        width={1600}
+        height={1600}
+        className="h-full w-full object-cover"
+        quality={100}
+        alt={project.title}
+      />
+      <div className="absolute bottom-0 p-4">
+        <ul className="flex gap-2 items-center flex-wrap">
+          {tags.map((tag, index) => (
+            <li key={index} className="">
+              <Badge>{tag}</Badge>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   )
 }
