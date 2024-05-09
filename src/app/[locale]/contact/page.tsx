d61d1904754b3fed5cbe9a5a644b134ab935bcf3
Filipe Vieira <filipe.silva@cidadaoon.com>
2024-05-09 17:24:57 -0300

    test globe three js

---
+++ src/app/[locale]/contact/page.tsx
@@ -0,0 +1,150 @@
+'use client'
+import React, { useEffect, useRef } from 'react'
+import ThreeGlobe from 'three-globe'
+import {
+  WebGLRenderer,
+  Scene,
+  PerspectiveCamera,
+  AmbientLight,
+  DirectionalLight,
+  PointLight,
+  Color,
+  Fog,
+  MeshPhongMaterial,
+  Vector2,
+} from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import MapaData from '@/assets/custom.geo.json'
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass'
+import { hslToHex } from '@/utils/hsl-to-hex'
+
+export default function Contact() {
+  const mountRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (!mountRef.current) return
+
+    let width = window.innerWidth
+    let height = window.innerHeight
+    let frameId: number
+
+    // Initialize renderer
+    const renderer = new WebGLRenderer({ antialias: true, alpha: true })
+    renderer.setPixelRatio(window.devicePixelRatio)
+    renderer.setSize(width, height, true)
+    mountRef.current.appendChild(renderer.domElement)
+
+    // Initialize scene
+    const scene = new Scene()
+    scene.add(new AmbientLight(0xbbbbbb, 0.3))
+    scene.background = new Color(hslToHex(240, 10, 3.9))
+
+    // Initialize camera
+    const camera = new PerspectiveCamera()
+    camera.aspect = width / height
+    camera.updateProjectionMatrix()
+
+    camera.position.z = 400
+    camera.position.x = 0
+    camera.position.y = 0
+
+    const dLight = new DirectionalLight(0xffffff, 1.5)
+    dLight.position.set(-800, 2000, 400)
+    camera.add(dLight)
+
+    const dLight1 = new DirectionalLight(0x7982f6, 1.5)
+    dLight1.position.set(-200, 500, 200)
+    camera.add(dLight1)
+
+    const dLight2 = new PointLight(0x8566cc, 1)
+    dLight2.position.set(-200, 500, 200)
+    camera.add(dLight2)
+
+    scene.fog = new Fog(0x535ef3, 400, 2000)
+
+    const controls = new OrbitControls(camera, renderer.domElement)
+    controls.enableDamping = true
+    controls.dynamicDampingFactor = 0.01
+    controls.enablePan = false
+    controls.minDistance = 200
+    controls.maxDistance = 500
+    controls.rotateSpeed = 0.8
+    controls.zoomSpeed = 1
+    controls.enableZoom = false
+    controls.autoRotate = false
+
+    controls.minPolarAngle = Math.PI / 3.5
+    controls.maxPolarAngle = Math.PI - Math.PI / 3
+
+    // Globe
+    const globe = new ThreeGlobe()
+      .hexPolygonsData(MapaData.features)
+      .hexPolygonResolution(3)
+      .hexPolygonMargin(0.7)
+      .showAtmosphere(true)
+      .atmosphereColor('#3a228a')
+      .atmosphereAltitude(0.25)
+
+    const globeMaterial = new MeshPhongMaterial()
+    globeMaterial.color = new Color(0x3a228a)
+    globeMaterial.emissive = new Color(0x220038)
+    globeMaterial.emissiveIntensity = 0.2
+    globeMaterial.shininess = 20
+    globe.globeMaterial(globeMaterial)
+
+    scene.add(globe)
+    scene.add(camera)
+
+    // Handle resizing
+    const handleResize = () => {
+      width = window.innerWidth
+      height = window.innerHeight
+      renderer.setSize(width, height)
+      camera.aspect = width / height
+      camera.updateProjectionMatrix()
+    }
+    const composer = new EffectComposer(renderer)
+    const renderPass = new RenderPass(scene, camera)
+    composer.addPass(renderPass)
+
+    const bloomPass = new UnrealBloomPass(
+      new Vector2(window.innerWidth, window.innerHeight),
+      1.5,
+      0.4,
+      0.85,
+    )
+    bloomPass.threshold = 0
+    bloomPass.strength = 0.8
+    bloomPass.radius = 1
+    composer.addPass(bloomPass)
+
+    // Animation loop
+    const animate = () => {
+      controls.update()
+      renderer.render(scene, camera)
+      composer.render()
+      frameId = window.requestAnimationFrame(animate)
+    }
+
+    // Start animation
+    renderer.setSize(width, height)
+    mountRef.current.appendChild(renderer.domElement)
+    window.addEventListener('resize', handleResize)
+    frameId = requestAnimationFrame(animate)
+
+    // Clean up
+    return () => {
+      window.cancelAnimationFrame(frameId)
+      window.removeEventListener('resize', handleResize)
+      mountRef.current?.removeChild(renderer.domElement)
+    }
+  }, [])
+
+  return (
+    <div className="w-screen overflow-hidden">
+      <div ref={mountRef} />
+    </div>
+  )
+}
