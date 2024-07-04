4c9dc5da5904f1b3c47adeb3bdeed2cd4e6824b9
Filipe Vieira <filipe.silva@bttis.com>
2024-07-04 17:31:37 -0300

    ✨ feat: guestbook migration

--- packages/drizzle/schema/sessions.ts
+++ packages/drizzle/schema/sessions.ts
@@ -1,3 +1,4 @@
+import { relations } from 'drizzle-orm'
 import { pgTable, text, timestamp } from 'drizzle-orm/pg-core'
 
 import { users } from './user'
@@ -9,3 +10,10 @@ export const sessions = pgTable('session', {
     .references(() => users.id, { onDelete: 'cascade' }),
   expires: timestamp('expires', { mode: 'date' }).notNull(),
 })
+
+export const sessionsRelations = relations(sessions, ({ one }) => ({
+  users: one(users, {
+    fields: [sessions.userId],
+    references: [users.id],
+  }),
+}))
--- packages/drizzle/schema/user.ts
+++ packages/drizzle/schema/user.ts
@@ -2,6 +2,7 @@ import { relations } from 'drizzle-orm'
 import { pgTable, text, timestamp } from 'drizzle-orm/pg-core'
 
 import { accounts } from './accounts'
+import { guestbooks } from './guestbook'
 import { sessions } from './sessions'
 
 export const users = pgTable('user', {
@@ -18,6 +19,9 @@ export const usersRelations = relations(users, ({ one, many }) => ({
   sessions: one(sessions, {
     fields: [users.id],
     references: [sessions.userId],
+  }),
+  guestbooks: many(guestbooks, {
+    relationName: 'author',
   }),
   accounts: many(accounts),
 }))
